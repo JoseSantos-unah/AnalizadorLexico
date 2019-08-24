77f9b5aed1184cd402ca92bebaf4bb8e04c63755
jose <joseeduardomadrid7@hotmail.com>
2019-08-24 04:34:12 -0500

    modificada la forma de lectura de archivos y algunos cambios en la BNF

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char aritmetic_operators[] = 
+const char* data_type[] = 
+{"int", "float", "double", "void", "char", "long"}; 
+
+const char* aritmetic_operators[] = 
 { "+", "*", "-", "/" };
 
-const char unary_operators[] = 
+const char* unary_operators[] = 
 { "--", "++", "-"};
 
-const char logical_operator[] =
+const char* logical_operator[] =
 { "<", ">", ">=", "<=", "==", "!=" };
 
 char assign = '=';
@@ -20,32 +23,67 @@ typedef enum
     tkn_string_literal
 }Tokens;
 
+typedef enum
+{
+    Variable,
+    Number,
+    String,
+    Reserved
+}States;
 
 
-char* getToken(const char letter);
+Tokens getToken(const char letter);
 
 void main(int* argv, char** args)
 {
     ++args;
 
-    int c;
-    FILE *file;
-    file = fopen(args[0], "r");
+    char * buffer = 0;
+    long length;
+    FILE * file = fopen (args[0], "r");
 
-    if(file)
+    if (file)
     {
-        while( (c = getc(file)) != EOF ){
-            putchar(c);
+        fseek (file, 0, SEEK_END);
+        length = ftell (file);
+        fseek (file, 0, SEEK_SET);
+
+        buffer = malloc (length);
+
+        if (buffer)
+        {
+            fread (buffer, 1, length, file);
         }
 
-        fclose(file);        
+        fclose (file);
     }
 
-
+    if (buffer)
+    {
+        printf("%s\n", buffer);
+    }
 }
 
 
-char* getToken(const char letter)
+Tokens getToken(const char letter)
 {
-   
+
+    States state;
+
+    switch (letter)
+    {
+        case '\n' | ' ' : ;
+
+        case 'a'|'b'|'c': state = Variable;
+
+        case '1'|'2'|'3': state = Number; 
+
+        case ' ' : break;
+
+        default:
+            break;
+   }
+
+
+
 }
